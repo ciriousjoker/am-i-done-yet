f2bc3b71aa7915bd849c9bbc8cf7295318b6bd47
CiriousJoker <ciriousjoker@gmail.com>
2019-03-26 12:00:45 +0100

    Cleaned up tests

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -2,10 +2,7 @@ import 'package:flutter_driver/driver_extension.dart';
 import 'package:am_i_done_yet/main.dart' as app;
 
 void main() {
-  // This line enables the extension
   enableFlutterDriverExtension();
 
-  // Call the `main()` function of your app or call `runApp` with any widget you
-  // are interested in testing.
   app.main();
 }
--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,23 +1,14 @@
-// Imports the Flutter Driver API
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Tests:', () {
-    // First, define the Finders. We can use these to locate Widgets from the
-    // test suite. Note: the Strings provided to the `byValueKey` method must
-    // be the same as the Strings we used for the Keys in step 1.
-    // final counterTextFinder = find.byValueKey('counter');
-    // final buttonFinder = find.byValueKey('Sign in with Google');
-
     FlutterDriver driver;
 
-    // Connect to the Flutter driver before running any tests
     setUpAll(() async {
       driver = await FlutterDriver.connect();
     });
 
-    // Close the connection to the driver after the tests have completed
     tearDownAll(() async {
       if (driver != null) {
         driver.close();
@@ -30,19 +21,23 @@ void main() {
       // => Human interaction: Login
 
       await driver.waitFor(find.text("Am I Done Yet"));
-      // driver.tap(find.text("Sign in with Google"));
-      // expect(await driver.getText(find.text("Sign in with Google")), );
     });
 
     test('Add list item', () async {
-      String itemTitle = "Some Item";
+      String itemTitle = "This isn't working. Read app_test.dart.";
       final textfield = find.byType("TextField");
       await driver.waitFor(textfield);
       await driver.tap(textfield);
       await driver.enterText(itemTitle);
       await driver.waitFor(find.text(itemTitle));
-      await driver.enterText("\x0D"); // Enter key
-      await driver.waitFor(find.text("adfgadfgadfg"));
+      // await driver.enterText("\x0D"); // Enter key doesn't submit it
+
+      // Here I want to submit the text, but there's no function for that.
+      // Related StackOverflow post:  https://stackoverflow.com/questions/55101120/submit-textfield-in-flutter-integration-test
+      // Related Github issue:        https://github.com/flutter/flutter/issues/29450
+      // Basically, I'm missing something like driver.submitTextfield(textfield)
+
+      await driver.waitFor(find.text("KEEP APP OPEN"));
     });
   });
 }
